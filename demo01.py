73f71d9005c388db19a6f011191f3fe52d3433e0
ghost7431 <596240433@qq.com>
2020-04-04 21:52:19 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,20 @@
+# print("ello world!") # 字符串
+# print(22222)  # 整数
+# print(3.2222) # 小数
+# print(True) # 布尔值
+# print(()) # 元祖
+# print([]) # 整数
+# print({}) # 字典
+# # 注释  """  ''' 注释 ''' """
+
+
+# print(type("haha")) # 字符串
+# print(type(2222))  # 整数
+# print(type(2.333)) # 小数
+# print(type(True)) # 布尔
+# print(type(()))  # 元祖
+# print(type([]))  # 数组  
+# print(type({}))  # 字典
+a=input("请输入第一段话：")
+b=input("请输入第二段话;")
+print("总长度：",len(a+b))
